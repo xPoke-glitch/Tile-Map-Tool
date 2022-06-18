6b833e2183a0681d9e7ea2f2c8c6a1c45e3ff669
Cristian <cri.ghiorzi@gmail.com>
2022-06-18 08:33:04 +0200

    arrow buttons

--- Assets/Scripts/CustomTools/AddTileSection.cs
+++ Assets/Scripts/CustomTools/AddTileSection.cs
@@ -64,8 +64,34 @@ public partial class MapEditor : EditorWindow
         EditorGUILayout.BeginHorizontal();
         _tilePoint.x = EditorGUILayout.IntField(_tilePoint.x);
         _tilePoint.y = EditorGUILayout.IntField(_tilePoint.y);
+        EditorGUILayout.EndHorizontal();
 
-        if(_tilePoint.x >= _tileMatrix.GetLength(0))
+        // Left and Right Arrows
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Left"))
+        {
+            _tilePoint.x--;
+        }
+        if (GUILayout.Button("Right"))
+        {
+            _tilePoint.x++;
+        }
+        EditorGUILayout.EndHorizontal();
+
+        // Up and Down Arrows
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Up"))
+        {
+            _tilePoint.y--;
+        }
+        if (GUILayout.Button("Down"))
+        {
+            _tilePoint.y++;
+        }
+        EditorGUILayout.EndHorizontal();
+
+
+        if (_tilePoint.x >= _tileMatrix.GetLength(0))
         {
             _tilePoint.x = _tileMatrix.GetLength(0)-1;
         }
@@ -78,7 +104,6 @@ public partial class MapEditor : EditorWindow
         if (_tilePoint.y < 0)
             _tilePoint.y = 0;
 
-        EditorGUILayout.EndHorizontal();
 
         // Add Button
         if (GUILayout.Button("Add Tile"))
@@ -91,6 +116,17 @@ public partial class MapEditor : EditorWindow
             _tileMatrix[_tilePoint.x, _tilePoint.y].TileObject = Instantiate(_tileMatrix[_tilePoint.x, _tilePoint.y].TileAssets, new Vector3(_tilePoint.x*2, 0, -_tilePoint.y*2), Quaternion.identity);
         }
 
+        // Remove Button
+        if (GUILayout.Button("Remove Tile"))
+        {
+            if (_tileMatrix[_tilePoint.x, _tilePoint.y].TileObject != null)
+            {
+                DestroyImmediate(_tileMatrix[_tilePoint.x, _tilePoint.y].TileObject);
+                _tileMatrix[_tilePoint.x, _tilePoint.y] = new Tile();
+            }
+        }
+
+        
         // Show where the Tile will be
         if (Event.current.type == EventType.Repaint)
         {
